845964c4caa4e4b2054bc445e7fbc2be3435ec5c
knapp <emiliodamasco04@gmail.com>
2023-05-09 07:20:49 -0700

    main entry function

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+int main(int argc, char *argv[])
+{
+    char *file;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = argv[1];
+
+    read_file(file);
+
+    return (0);    
+}
