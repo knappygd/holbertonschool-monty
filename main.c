912e7434183bee2782fe564b03503092758c22af
Aldo2303 <aldod03@gmail.com>
2023-05-12 09:01:56 -0700

    push

--- main.c
+++ main.c
@@ -2,26 +2,3 @@
 
 int main(void)
 {
-    stack_t *stack = NULL;
-    /* char *file;
-
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-
-    file = argv[1];
-
-    read_file(file); */
-
-    push(&stack, 5);
-    push(&stack, 7);
-    push(&stack, 1);
-    push(&stack, 3);
-    pall(&stack);
-    pop(&stack);
-    pall(&stack);
-
-    return (0);    
-}
