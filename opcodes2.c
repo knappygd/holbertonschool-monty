78d3cbb6e38a9c8650a875de66cf662f4932c139
Aldo2303 <aldod03@gmail.com>
2023-05-12 15:33:02 -0700

    changed nop function

--- opcodes2.c
+++ opcodes2.c
@@ -31,6 +31,7 @@ void swap(stack_t **stack, unsigned int line_number)
 */
 void nop(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+    (void) stack;
+    (void) line_number;
 }
+
