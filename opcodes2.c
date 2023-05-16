3e6acb54cc800fa3d9af0261b9a7e202c6882033
knapp <emiliodamasco04@gmail.com>
2023-05-16 09:50:07 -0700

    added mul

--- opcodes2.c
+++ opcodes2.c
@@ -79,3 +79,23 @@ void dvs(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+
+/**
+ * mul - Multiplies the value of the two last nodes.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: The line on which the command is executed.
+*/
+void mul(stack_t **stack, unsigned int line_number)
+{
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n *= (*stack)->n;
+	(*stack) = (*stack)->next;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
