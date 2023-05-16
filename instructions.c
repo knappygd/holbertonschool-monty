5b2cc262965ff69d1b5a1573085e83b6610f687f
knapp <emiliodamasco04@gmail.com>
2023-05-16 09:36:14 -0700

    fixed betty

--- instructions.c
+++ instructions.c
@@ -1,11 +1,11 @@
 #include "monty.h"
+
 /**
  * get_instructions - Holds an array containing the opcode and the functions.
  * Return: The corresponding array.
 */
 instruction_t *get_instructions()
 {
-	/*static reserves memory while the program is active*/
 	static instruction_t arr[] = {
 		{"push", push},
 		{"pall", pall},
@@ -14,6 +14,7 @@ instruction_t *get_instructions()
 		{"add", add},
 		{"swap", swap},
 		{"nop", nop},
+		{"sub", sub},
 		{NULL, NULL}};
 	return (arr);
 }
--- opcode_handler.c
+++ opcode_handler.c
@@ -1,16 +1,17 @@
 #include "monty.h"
 
 /**
- * read_file - Handles the content of the file passed.
- * @file: The file (of type FILE *) previously read in main().
- * Return: 0 if successful
+ * read_file - Handles the content of te  file passed.
+ * @file: The file (of type FILE *) previously read in main.
+ * 
+ * Return: 0 if successful.
 */
 int read_file(FILE *file)
 {
 	char *buffer = NULL, *token = NULL;
 	stack_t *stack = NULL;
 	size_t size = 0;
-	unsigned int line_number = 0, i = 0, flag = 0;
+	unsigned int line_number = 0, i, flag = 0;
 	instruction_t *arr = get_instructions();
 
 	while (1)
@@ -20,23 +21,21 @@ int read_file(FILE *file)
 			free(buffer);
 			break;
 		}
+
 		line_number++;
 		flag = 0;
-		token = strtok(buffer, " \t\n");
-		if (token != NULL)
+
+		token  = strtok(buffer, " \t\n");
+		if (token)
 		{
-			for (i = 0; arr[i].opcode != NULL; i++)
-			{
+			for (i = 0; arr[i].opcode; i++)
 				if (strcmp(token, arr[i].opcode) == 0)
-				{
-					flag = 1;
-					arr[i].f(&stack, line_number);
-				}
-			}
+					arr[i].f(&stack, line_number), flag = 1;
 			if (flag == 0)
 			{
 				fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
-				free(buffer), free_stack(stack), fclose(file), exit(EXIT_FAILURE);
+				free(buffer), free_stack(stack), fclose(file);
+				exit(EXIT_FAILURE);
 			}
 		}
 	}
